dff509d9ef46e37aa984ddf04f93d720b04d1fe4
songhanjian <hjsong1993@163.com>
2017-03-29 01:01:22 +0800

    dataAccess 修改提交

--- super-schedule-model/src/main/java/com/su/schedule/model/po/DataAccess.java
+++ super-schedule-model/src/main/java/com/su/schedule/model/po/DataAccess.java
@@ -8,23 +8,93 @@ public class DataAccess {
 	private String period;
 	private String teacherName;
 	private String courseName;
+	private Integer courseId;
+	private Integer teacherId;
+
 	
 	public DataAccess() {
 	}
-	
+
+
 	@Override
-	public String toString(){
-		return "Relation{" +
-                "id=" + id +
-                ", weekNo=" + weekNo +
-                ", lessonNo=" + lessonNo +
-                ", className='" + className + '\'' +
-                ", period=" + period + '\'' +
-                ", teacherName=" + teacherName + '\'' +              
-                ", buildingName='" + courseName + '\'' +
-                '}';
+	public String toString() {
+		return "DataAccess{" +
+				"id=" + id +
+				", weekNo=" + weekNo +
+				", lessonNo=" + lessonNo +
+				", className='" + className + '\'' +
+				", period='" + period + '\'' +
+				", teacherName='" + teacherName + '\'' +
+				", courseName='" + courseName + '\'' +
+				", courseId=" + courseId +
+				", teacherId=" + teacherId +
+				'}';
 	}
-	
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		DataAccess that = (DataAccess) o;
+
+		if (id != null ? !id.equals(that.id) : that.id != null) return false;
+		if (weekNo != null ? !weekNo.equals(that.weekNo) : that.weekNo != null) return false;
+		if (lessonNo != null ? !lessonNo.equals(that.lessonNo) : that.lessonNo != null) return false;
+		if (className != null ? !className.equals(that.className) : that.className != null) return false;
+		if (period != null ? !period.equals(that.period) : that.period != null) return false;
+		if (teacherName != null ? !teacherName.equals(that.teacherName) : that.teacherName != null) return false;
+		if (courseName != null ? !courseName.equals(that.courseName) : that.courseName != null) return false;
+		if (courseId != null ? !courseId.equals(that.courseId) : that.courseId != null) return false;
+		return teacherId != null ? teacherId.equals(that.teacherId) : that.teacherId == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = id != null ? id.hashCode() : 0;
+		result = 31 * result + (weekNo != null ? weekNo.hashCode() : 0);
+		result = 31 * result + (lessonNo != null ? lessonNo.hashCode() : 0);
+		result = 31 * result + (className != null ? className.hashCode() : 0);
+		result = 31 * result + (period != null ? period.hashCode() : 0);
+		result = 31 * result + (teacherName != null ? teacherName.hashCode() : 0);
+		result = 31 * result + (courseName != null ? courseName.hashCode() : 0);
+		result = 31 * result + (courseId != null ? courseId.hashCode() : 0);
+		result = 31 * result + (teacherId != null ? teacherId.hashCode() : 0);
+		return result;
+	}
+
+	public String getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public Integer getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Integer courseId) {
+		this.courseId = courseId;
+	}
+
+	public Integer getTeacherId() {
+		return teacherId;
+	}
+
+	public void setTeacherId(Integer teacherId) {
+		this.teacherId = teacherId;
+	}
+
 	public DataAccess(Integer id, Integer weekNo, Integer lessonNo, String className, String peroid, String teacherName, String buildingName){
 		this.id = id;
 		this.weekNo = weekNo;
