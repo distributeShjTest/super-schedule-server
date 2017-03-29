0b48bcea0a902e3a1732ac7312e5e2f63b35e055
songhanjian <hjsong1993@163.com>
2017-03-29 23:42:00 +0800

    spy接口上传

---
+++ super-schedule-spy/src/main/java/com/su/schedule/spy/handler/PeizhengHandler.java
@@ -0,0 +1,21 @@
+package com.su.schedule.spy.handler;
+
+import com.su.schedule.model.dto.DataSend;
+import com.su.schedule.model.vo.TimeTableModel;
+
+import java.util.List;
+
+/**
+ * Created by shj on 2017/3/29.
+ */
+public interface PeizhengHandler {
+
+     List<TimeTableModel> queryTimetable(DataSend dataSend)throws Exception;
+
+     byte[] getValidatePic()throws Exception;
+
+
+     boolean insertValidateCode(String code)throws Exception;
+
+
+}
